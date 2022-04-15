cb9800e823ad7efc81d6be9e7b2ab1dec9644a81
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2022-04-15 14:35:52 +0200

    Alpha-beta prunning

--- gomoengine/__main__.py
+++ gomoengine/__main__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 from typing import List, Tuple
-from os import path
-from random import randint
+from sys import argv
 
-from heuristics import get_rating
+from .heuristics import get_rating
 
 BOARD_SIZE = 20
 
@@ -27,35 +26,12 @@ def read_moves(filepath: str) -> Tuple[List[List[int]], int]:
     return (board, max_player)
 
 
-def write_move(filepath, x, y):
-    with open(filepath, "w+") as file:
-        file.write(f"{x} {y}")
-
-
-def set_rand_move(board: List[List[int]]) -> Tuple[int, int]:
-    num_empty = sum([i.count(0) for i in board])
-
-    if num_empty == 0:
-        return (-1, -1)
-
-    num = randint(0, num_empty)
-
-    for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            if board[i][j] == 0:
-                if not num:
-                    return (i, j)
-                num -= 1
-
-    return (-1, -1)
-
-
 def print_board(board):
     for i in board:
         print(i)
 
 
-def get_next_minmax_move(board, player, alpha, beta, depth=1, is_max=True) -> Tuple[int, int, int]:
+def get_next_minmax_move(board, player, alpha, beta, depth=3, is_max=True) -> Tuple[float, int, int]:
     if depth == 0:
         return get_rating(board, player), -1, -1
 
@@ -65,29 +41,43 @@ def get_next_minmax_move(board, player, alpha, beta, depth=1, is_max=True) -> Tu
     else:
         best_score = float("inf")
 
+    # TODO: Refactor this mess
     for i in range(BOARD_SIZE):
         for j in range(BOARD_SIZE):
             if board[i][j] == 0:
                 board[i][j] = player
-                score, _, _ = get_next_minmax_move(board, player, depth - 1, not is_max)
+                score, _, _ = get_next_minmax_move(
+                    board, player, alpha, beta, depth - 1, not is_max
+                )
                 board[i][j] = 0
 
                 if is_max and score > best_score or not is_max and score < best_score:
                     x, y = i, j
                     best_score = score
-
-    return int(best_score), x, y
+                
+                if is_max:
+                    if score >= beta:
+                        break
+                    alpha = max(alpha, score)
+                else:
+                    if score <= alpha:
+                        break
+                    beta = min(beta, score)
+        else:
+            continue
+        break
+                
+    
+    return best_score, x, y
 
 
 if __name__ == "__main__":
-    moves_path = path.join(path.dirname(__file__), "moves.txt")
-    play_path = path.join(path.dirname(__file__), "play.txt")
+    if len(argv) != 2:
+        print(f"Usage: {argv[0]} [filepath of moves.txt]")
+        exit(1)
 
-    board, player = read_moves(moves_path)
+    board, player = read_moves(argv[1])
 
-    print_board(board)
+    score, x, y = get_next_minmax_move(board, player, float("-inf"), float("inf"))
 
-    # x, y = set_rand_move(board)
-    score, x, y = get_next_minmax_move(board, player)
-
-    write_move(play_path, x, y)
+    print(x, y)
